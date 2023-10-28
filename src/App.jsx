d0dc2d20bf0d621ca213838b9d60d35add084fad
loukman-dare <kiogymy@gmail.com>
2023-10-28 15:25:17 +0200

    commit files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./components/user/Langing/Landing";
 import UserLayout from "./components/user/layout/UserLayout";
 import ContactUs from "./components/user/CantactUs/ContactUs";
-import Register from "./components/user/Auth/Register/Register"
+
 function App() {
     return (
         <div className="font-alegreya">
@@ -10,7 +10,7 @@ function App() {
                 <Routes>
                     <Route path="/login" element={<></>} />
                     <Route path="/" element={<UserLayout />}>
-                        <Route path="/" element={<Register />} />
+                        <Route path="/" element={<Landing />} />
                         <Route path="/services" element={<></>} />
                         <Route path="/about-us" element={<></>} />
                         <Route path="/contact-us" element={<ContactUs/>} />
